f69d384ba3c1d1ae6eb976df32ee212590110f31
RobinBigus <robin.bigus@wbscodingschool.com>
2025-10-10 11:16:14 +0200

    added js-recap

---
+++ module02-web-apis/js-recap/script.js
@@ -0,0 +1,78 @@
+// 1. What’s the difference between let and const?
+
+// const string = "hello";
+// string = "moin";
+
+// let number;
+// number = 11;
+
+// 2. Which data types exist in JS?
+// Primitive Types: Number, String,  Boolean, Undefined, Null (BigInt, Symbol);
+// Non-Primitive Types: Array, Object, Function
+
+// 3. Difference between === and == check?
+// == checks for value
+// === checks for type & value
+
+// 4. How do you create a function that returns “Hello”?
+
+// function sayHello() {
+//   return "Hello";
+// }
+
+// const sayHello2 = () => "Hello";
+
+// 5. What’s the output of 5 + "5"? Why?
+
+// → "55" (number and string → string concatenation)
+
+// 6. How do you write a ternary expression to check if someone is an adult?
+// age = 18;
+// const isAdult = age >= 18 ? "Yes" : "No";
+
+// 7. How do you loop through numbers 0 to 4 and console log them using a for loop?
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+
+// 8. What does break do in a loop?
+// → It exits the loop immediately.
+
+// 9. How do you handle errors in JavaScript within async operations?
+// try {
+
+// } catch (error) {
+
+// }
+
+// 10. What does !false return?
+
+// → true
+
+// 11. How do you define an array with 3 items and access the second one?
+
+// let array = [1, 2, 3];
+// console.log(array[1]);
+
+// 12. How do you add an element to the end of an array?
+
+// array.push(4);
+// console.log(array);
+
+// 13. How do you define an object with properties name and age?
+
+// const person = {
+//   name: "John",
+//   age: 20,
+// };
+
+// 14. What’s the main structural difference between an object and an array?
+
+// → Arrays are ordered lists; objects are unordered key-value pairs.
+
+// 15. What are logical operators and what is the difference between them?
+
+// → && (AND) Returns true only if both operands are true
+// → || (OR) Returns true if at least one operand is true
+// → ! (NOT) Inverts the Boolean value: true becomes false, and vice versa
